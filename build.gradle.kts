598bf670a413f56c009c17e3f9678778c709b42b
omar-abdelaziz-mersal <bsbsbsbbsbs@swiftyomail.org>
2026-08-29 05:45:28 +0300

    Refactor build.gradle.kts and update Kotlin version
    Removed plugin and dependency resolution management sections. Updated Kotlin plugin version from 1.9.20 to 1.9.10.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,10 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
+// Top-level build file
 plugins {
     id("com.android.application") version "8.1.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.android.library") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
 
-tasks.register("clean", Delete::class) {
+task<Delete>("clean") {
     delete(rootProject.buildDir)
 }
