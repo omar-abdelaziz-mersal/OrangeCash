3b859322db0d02dbbb7379bc1af8b105131fcb0c
omar-abdelaziz-mersal <bsbsbsbbsbs@swiftyomail.org>
2026-08-29 05:09:16 +0300

    Add color definitions for UI theme

---
+++ app/src/main/java/com/orangecash/app/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.orangecash.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val OrangeMain = Color(0xFFFF7900)
+val OrangeDark = Color(0xFFE66000)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val DarkNavy = Color(0xFF1A1F36)
+val DarkGray = Color(0xFF222222)
+val Gray = Color(0xFF666666)
+val LightGray = Color(0xFFF5F5F5)
+val BorderGray = Color(0xFFE0E0E0)
+val SuccessGreen = Color(0xFF16A34A)
+val ErrorRed = Color(0xFFDC2626)
+val WarningAmber = Color(0xFFF59E0B)
