bdd9593d8d2e69e46c9717bf10e70608ee705f9b
omar-abdelaziz-mersal <bsbsbsbbsbs@swiftyomail.org>
2026-08-29 05:35:25 +0300

    Configure plugin and dependency resolution management

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,19 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
